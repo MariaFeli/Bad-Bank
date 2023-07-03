84c8c5a67337e4820768dd8f19605e53fecd2b76
Maria <122727669+MariaFeli@users.noreply.github.com>
2023-07-03 20:34:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+const Route       = ReactRouterDOM.Route;
+const Link        = ReactRouterDOM.Link;
+const HashRouter  = ReactRouterDOM.HashRouter;
+// create context
+const UserContext = React.createContext(null);
+
 function Spa() {
   return (
-      <h1>Welcome to Bad Bank</h1>
-     <HashRouter>
+ <HashRouter>
+     <div>
+    <h1>Routing - Welcome to Bad Bank</h1>
+    <div className="container" style={{padding: "20px"}}>
+          <Link to="/">Home</Link> -- 
+          <Link to="/about/">About</Link> -- 
+          <Link to="/products">Products</Link>
+          <hr/>
       <NavBar/>
      <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>   
-   <div className="container" style={{padding: "20px"}}>
+            <UserContext.Provider value={{users:['peter']}}>
+            <Route path="/" exact    component={Home}     />
+            <Route path="/about/"    component={About}    />          
+            <Route path="/products/" component={Products} />
       <Route path="/" exact component={Home} />
       <Route path="/CreateAccount/" component={CreateAccount} />
       <Route path="/login/" component={Login} />
@@ -23,4 +38,5 @@ ReactDOM.render(
   
   );
 }
+
 
