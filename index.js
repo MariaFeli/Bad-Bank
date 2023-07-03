66b20a72c5c354ff8fefe427446f012251e6b0f6
Maria <122727669+MariaFeli@users.noreply.github.com>
2023-07-03 16:58:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function Spa() {
 ReactDOM.render(
   <Spa/>,
   document.getElementById('root')
+
+
+    <HashRouter>
+      <NavBar/>        
+      <Route path="/" exact component={Home} />
+      <Route path="/CreateAccount/" component={CreateAccount} />
+      <Route path="/login/" component={Login} />
+      <Route path="/deposit/" component={Deposit} />
+      <Route path="/withdraw/" component={Withdraw} />
+      <Route path="/balance/" component={Balance} />
+      <Route path="/alldata/" component={AllData} />
+    </HashRouter>
+  );
+}
+
+ReactDOM.render(
+  <Spa/>,
+  document.getElementById('root')
 );
