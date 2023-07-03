6e847aa6220b2ed2274ce6cbad163853a5f70ad8
Maria <122727669+MariaFeli@users.noreply.github.com>
2023-07-03 17:50:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ ReactDOM.render(
 
     <HashRouter>
       <NavBar/>        
-   <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>      
+   <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>   
+   <div className="container" style={{padding: "20px"}}>
       <Route path="/" exact component={Home} />
       <Route path="/CreateAccount/" component={CreateAccount} />
       <Route path="/login/" component={Login} />
@@ -21,6 +22,8 @@ ReactDOM.render(
       <Route path="/withdraw/" component={Withdraw} />
       <Route path="/balance/" component={Balance} />
       <Route path="/alldata/" component={AllData} />
+   </div>                    
+      </UserContext.Provider> 
     </HashRouter>
   );
 }
